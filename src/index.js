12b63dd078d6358c3eefcbd02b3d524eff6d5bb3
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-05-16 18:00:23 -0300

    Configuração de  acesso api

--- src/index.js
+++ src/index.js
@@ -2,28 +2,20 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
 import { BrowserRouter, Route } from 'react-router-dom'
+import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import reducers from './reducers';
+import PostsIndex from './components/posts_index'
 
-class Hello extends Component {
-  render() { return <div> Hello !! </div> }
-}
 
-class Goodbye extends Component {
-  render() { return <div> Goodbye !! </div> }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/hello' component={Hello} />
-        <Route path='/goodbye' component={Goodbye} />
+        <Route path='/' component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
